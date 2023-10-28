bf7bb003a94970b007112d8d19512004d3ec3462
johnny-kiv <johnny.kiv@ya.ru>
2023-10-28 10:56:18 +0300

    Card with task is ready

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return render_template('panel.html')
+def main():
+    return render_template('panel.html',href="http://192.168.1.11:5000/1")
+@app.route("/<number>")
+def one(number):
+    return render_template(f'tasks-descriptions/{number}.html')
 
 if __name__ == '__main__':
     app.run("0.0.0.0")
