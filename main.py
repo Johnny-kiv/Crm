db2bca45e3524322986f06c7535a13435df26c3f
johnny-kiv <johnny.kiv@ya.ru>
2023-11-12 11:05:22 +0300

    i'm in progress design

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
     res_list = []
     for f in files:
         path = "client/0001/tasks/"+f
-        href = "http://192.168.1.11:5000/"+f.split("task_")[1]
+        href = "window.location.href = '"+"http://192.168.1.11:5000/"+f.split("task_")[1].split(".xml")[0]+"';"
         number = f.split("task_")[1]
         name = xml("title",path)
         d = xml("description",path)
@@ -29,12 +29,21 @@ def main():
                 data = elem.attrib['expiration']
                 status = elem.attrib['status_payment']
         d1 = {"href":href, "name":name, "descr":descr,"author":executor,"data":data,"status":status}
-        print(d1)
         res_list.append(d1)
     return render_template('panel.html',res_list=res_list)
 @app.route("/<number>")
 def one(number):
-    return render_template(f'tasks-descriptions/{number}.html')
+    path = f"client/0001/tasks/task_1.xml"
+    name = xml("title",path)
+    descr = xml("description",path)
+    root_node = ET.parse(path).getroot()
+    for elem in root_node.iter():
+        if elem.tag == "data":
+            executor = elem.attrib['executor']
+            data = elem.attrib['expiration']
+            status = elem.attrib['status_payment']
+    d = {"name":name, "descr":descr,"author":executor,"data":data,"status":status}
+    return render_template(f'tasks-descriptions/{number}.html',name=name, descr=descr,author=executor,data=data,status=status)
 
 
 if __name__ == '__main__':
